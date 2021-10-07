0994aab5e7be0e4aee683a0ee67a99eae13bee8a
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-10-07 16:39:20 +0600

    product add cart

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -8,6 +8,7 @@ import {
   Typography,
 } from "@material-ui/core";
 import NextLink from "next/link";
+// import useRouter from "next/router";
 import React, { useContext } from "react";
 import Image from "next/image";
 import Layout from "../../components/Layout";
@@ -16,8 +17,10 @@ import Product from "../../models/Product";
 import db from "../../utils/db";
 import axios from "axios";
 import { Store } from "../../utils/Store";
+import { useRouter } from "next/router";
 
 export default function ProductScreen(props) {
+  const router = useRouter();
   const { dispatch } = useContext(Store);
   const { product } = props;
   const classes = useStyles();
@@ -27,11 +30,12 @@ export default function ProductScreen(props) {
 
   const addToCartHandler = async () => {
     const { data } = await axios.get(`/api/products/${product._id}`);
-    if(data.countInStock <= 0) {
-      window.alert('Sorry. Product is out of stock')
+    if (data.countInStock <= 0) {
+      window.alert("Sorry. Product is out of stock");
       return;
     }
-    dispatch({type: 'CART_ADD_ITEM', payload: {...product, quantity: 1}})
+    dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity: 1 } });
+    router.push("/cart");
   };
 
   return (
