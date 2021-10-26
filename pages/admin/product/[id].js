af37b4656df4a5f80277d44c724ee2382a08f4f2
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-10-26 12:16:44 +0600

    user to admin

--- pages/admin/product/[id].js
+++ pages/admin/product/[id].js
@@ -4,25 +4,12 @@
 import axios from "axios";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
-import NextLink from "next/link";
 import React, { useEffect, useContext, useReducer } from "react";
-import {
-  Grid,
-  List,
-  ListItem,
-  Typography,
-  Card,
-  Button,
-  ListItemText,
-  TextField,
-  CircularProgress,
-} from "@material-ui/core";
 import { getError } from "../../../utils/error";
 import { Store } from "../../../utils/Store";
 import Layout from "../../../components/Layout";
 import useStyles from "../../../utils/styles";
 import { Controller, useForm } from "react-hook-form";
-import { useSnackbar } from "notistack";
 
 function reducer(state, action) {
   switch (action.type) {
